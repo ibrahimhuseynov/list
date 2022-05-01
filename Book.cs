796d1a65ecf2364918bcfa38202dbc8f0764164c
ibrahimhuseynov <99806021+ibrahimhuseynov@users.noreply.github.com>
2022-05-01 08:39:40 +0400

    Add files via upload

---
+++ Book.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Book
+    {
+        public int _code=0;
+        private string _title;
+        public string Name { get; set; }
+        public string AuthorName { get; set; }
+        public int PageCount { get; set; }  
+         public string Code
+        {
+            get
+            {
+                return _title;
+            }
+            set
+            {
+                _title = check(value);
+            }
+        }
+        public string check(string value)
+        {
+            return (char.ToUpper(Name[0])+char.ToUpper(Name[1]) +_code).ToString(); 
+        }
+       
+    }
+      
+
+}
---
+++ Libary.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Libary
+    {
+       List<Book> Books = new List<Book>();
+        public List<Book> FindAllBooksByName(string name)
+        {
+            return Books.FindAll(x=>x.Name.Contains(name));
+        }
+        public void RemoveAllBooksByNme(string name)
+        {
+            Books.RemoveAll(x => x.Name.Contains(name));
+        }
+        public List<Book> SearchBooks(string name)
+        {
+            return Books.FindAll(x => x.Name.Contains(name) || x.AuthorName.Contains(name));
+        }
+        public List<Book> FindAllBooksByPageCountRange(int min, int max)
+        {
+            return Books.FindAll(x=>x.PageCount>=min&&x.PageCount<=max);
+        }
+        public void RemoveBookByCode(string name)
+        {
+            Book result=Books.Find(x=>x.Code==name);   
+        }
+
+    }
+}
